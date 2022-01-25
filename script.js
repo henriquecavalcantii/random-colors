ec3ece8822b89fdd0afcfcb5511dea50076a4b3a
henriquecavalcantii <henrique_inacio1159@hotmail.com>
2022-01-25 19:06:02 -0300

    main project

---
+++ script.js
@@ -0,0 +1,25 @@
+const box1 = document.querySelector("#col1");
+const box2 = document.querySelector("#col2");
+const box3 = document.querySelector("#col3");
+const setBg1 = () => {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    box1.style.background = "#" + randomColor;
+    col1.innerHTML = "#" + randomColor;
+}
+
+const setBg2 = () => {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    box2.style.background = "#" + randomColor;
+    col2.innerHTML = "#" + randomColor;
+}
+
+const setBg3 = () => {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    box3.style.background = "#" + randomColor;
+    col3.innerHTML = "#" + randomColor;
+}
+btncolor.addEventListener("click", () => {
+    setBg1();
+    setBg2();
+    setBg3();
+})
