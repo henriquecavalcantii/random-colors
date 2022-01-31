0bac746e6bf093dd548a29d73e3c1ebb3ca45e2b
henriquecavalcantii <henrique_inacio1159@hotmail.com>
2022-01-31 18:26:07 -0300

    Fix copying

--- script.js
+++ script.js
@@ -4,22 +4,52 @@ const box3 = document.querySelector("#col3");
 const setBg1 = () => {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     box1.style.background = "#" + randomColor;
-    col1.innerHTML = "#" + randomColor;
+    txt1.innerHTML = "#" + randomColor;
 }
 
 const setBg2 = () => {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     box2.style.background = "#" + randomColor;
-    col2.innerHTML = "#" + randomColor;
+    txt2.innerHTML = "#" + randomColor;
 }
 
 const setBg3 = () => {
     var randomColor = Math.floor(Math.random() * 16777215).toString(16);
     box3.style.background = "#" + randomColor;
-    col3.innerHTML = "#" + randomColor;
+    txt3.innerHTML = "#" + randomColor;
 }
 btncolor.addEventListener("click", () => {
     setBg1();
     setBg2();
     setBg3();
 })
+
+function copyToClipBoard1() {
+
+    var content = document.getElementById('txt1');
+
+    content.select();
+    document.execCommand('copy');
+
+    alert("Copied!");
+}
+
+function copyToClipBoard2() {
+
+    var content = document.getElementById('txt2');
+
+    content.select();
+    document.execCommand('copy');
+
+    alert("Copied!");
+}
+
+function copyToClipBoard3() {
+
+    var content = document.getElementById('txt3');
+
+    content.select();
+    document.execCommand('copy');
+
+    alert("Copied!");
+}
